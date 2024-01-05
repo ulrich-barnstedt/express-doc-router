23c3fd7b30a8e0602ad3d247e637c637d8aefc5c
ulrich-barnstedt <ulrich.barnstedt@gmail.com>
2024-01-05 20:46:55 +0100

    Create exports for NPM

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {AutoRouter} from "./AutoRouter";
+import {meta, MetaRouter} from "./MetaMiddleware";
+import {SpecificationGenerator} from "./SpecificationGenerator";
+import type {MetaRouterType, MetaFunction} from "./MetaMiddleware";
+
+export type {MetaFunction, MetaRouterType}
+export {AutoRouter, meta, MetaRouter, SpecificationGenerator}
